b2b35b224ae33c390e7cbe8d2bdf3ba9d9d28ecf
Pau Andrio <andriopau@gmail.com>
2021-06-17 21:59:21 +0200

    Feat (cmip): Adding cmip module

--- biobb_cmip/cmip/common.py
+++ biobb_cmip/cmip/common.py
@@ -16,8 +16,19 @@ def get_pdb_total_charge(pdb_file_path: str):
     return total_charge
 
 
-def params_grid(grid_type: str = 'cmip', pbfocus: bool = False, perfill: float = 0.8,
-                grid_int: Sequence[float] = (0.5, 0.5, 0.5), grid_dim: Sequence[float] = (64, 64, 64),
+def probe_params_grid(probe_id: int = 0, readgrid: int = 2, pbfocus: int = 1, perfill: float = 0.6,
+                      grid_int: Sequence[float] = (0.5, 0.5, 0.5)) -> Dict[str, str]:
+    grid_dict = {}
+    grid_dict[f"readgrid{probe_id}"] = f"{readgrid}"
+    grid_dict[f"perfill{probe_id}"] = f"{perfill}"
+    grid_dict['pbfocus'] = f"{pbfocus}"
+    grid_dict['grid_int'] = f"INTX{probe_id}={grid_int[0]},INTY{probe_id}={grid_int[1]},INTZ{probe_id}={grid_int[2]}"
+
+    return grid_dict
+
+def params_grid(grid_type: str = 'cmip', readgrid: int = 0, perfill: float = 0.8,
+                grid_int: Sequence[float] = (0.5, 0.5, 0.5),
+                grid_dim: Sequence[float] = (64, 64, 64),
                 grid_cen: Sequence[float] = (0.0, 0.0, 0.0)) -> Dict[str, str]:
     # grid_type older readgrid equivalences:
     #     2 proteina dist minima pecentatge, 4 distancia minima prot, 5 distancia al centre de masses
@@ -28,18 +39,17 @@ def params_grid(grid_type: str = 'cmip', pbfocus: bool = False, perfill: float =
     grid_dict = {}
 
     pbfocus = '0' if pbfocus else ''
-    readgrid = '0' if grid_type == 'interaction' else '2'
+    readgrid = '0' if grid_type in ['interaction' or 'energy'] else '2'
 
-    grid_dict[f"READGRID{pbfocus}"] = f"{readgrid}"
+    grid_dict[f"readgrid"] = f"{readgrid}"
 
-    if grid_type == 'interaction':
-        grid_dict['grid_cen'] = f"CENX{pbfocus}={grid_cen[0]},CENY{pbfocus}={grid_cen[1]},CENZ{pbfocus}={grid_cen[2]}"
-        grid_dict['grid_dim'] = f"DIMX{pbfocus}={grid_dim[0]},DIMY{pbfocus}={grid_dim[1]},DIMZ{pbfocus}={grid_dim[2]}"
-        grid_dict['grid_int'] = f"INTX{pbfocus}={grid_int[0]},INTY{pbfocus}={grid_int[1]},INTZ{pbfocus}={grid_int[2]}"
-    else:
-        grid_dict[f"PERFILL{pbfocus}"] = f"{perfill}"
-        grid_dict[
-            'grid_int'] = f"INTX{pbfocus}={grid_int[0]},INTY{pbfocus}={grid_int[1]},INTZ{pbfocus}={grid_int[2]}"
+    if grid_type in ['interaction', 'mip', 'energy', 'docking']:
+        grid_dict['grid_cen'] = f"CENX={grid_cen[0]},CENY={grid_cen[1]},CENZ={grid_cen[2]}"
+        grid_dict['grid_dim'] = f"DIMX={grid_dim[0]},DIMY={grid_dim[1]},DIMZ={grid_dim[2]}"
+        grid_dict['grid_int'] = f"INTX={grid_int[0]},INTY={grid_int[1]},INTZ={grid_int[2]}"
+    elif grid_type in ['solvation', 'titration']:
+        grid_dict['perfill'] = f"{perfill}"
+        grid_dict['grid_int'] = f"INTX={grid_int[0]},INTY={grid_int[1]},INTZ={grid_int[2]}"
 
     return grid_dict
 
@@ -47,29 +57,100 @@ def params_grid(grid_type: str = 'cmip', pbfocus: bool = False, perfill: float =
 def params_preset(execution_type: str) -> Dict[str, str]:
     params_dict = {}
     grid_dict = {}
+    probe_grid_dict = {}
     if execution_type == 'titration':
-        grid_dict = params_grid(grid_type='titration')
+        grid_dict = params_grid(grid_type='titration', readgrid=2, perfill=0.8, grid_int=(0.5, 0.5, 0.5) )
         params_dict = {
             'title': 'Titration',
             'tipcalc': 1,
-            'titration': 1,
-            'inifoc': 2,
-            'cutfoc': -0.5,
-            'focus': 1,
-            'ninter': 10,
-            'dields': 2,
-            'clhost': 1,
             'calcgrid': 1,
             'irest': 0,
             'orest': 0,
             'coorfmt': 2,
-            'titwat': 10,
-            'titip': 10,
-            'titim': 10,
-            'titcut': 20.
+            'dields': 2,
+            'titration': 1, 'inifoc': 2, 'cutfoc': -0.5, 'focus': 1, 'ninter': 10, 'clhost': 1, 'titcut': 20.,
+            'titwat': 10, 'titip': 10, 'titim': 10
+        }
+    elif execution_type == 'mip':
+        grid_dict = params_grid(grid_type='mip', readgrid= 0,
+                                grid_cen=(47.266, 83.4265, 54.174),
+                                grid_dim=(46, 30, 37),
+                                grid_int=(0.5, 0.5, 0.5))
+        params_dict = {
+            'title': 'MIP O-  Mehler Solmajer dielectric',
+            'tipcalc': 0,
+            'calcgrid': 1,
+            'irest': 0,
+            'orest': 0,
+            'coorfmt': 2,
+            'dields':  2,
+            'cubeoutput': 1, 'carmip': -1, 'fvdw': 0.8
         }
 
-    return {**params_dict, **grid_dict}
+    elif execution_type == 'solvation':
+        grid_dict = params_grid(grid_type='solvation', readgrid=2, perfill=0.2,
+                                grid_int=(0.5, 0.5, 0.5))
+        params_dict = {
+            'title': 'Solvation & MEP',
+            'tipcalc': 0,
+            'calcgrid': 1,
+            'irest': 0,
+            'orest': 0,
+            'coorfmt': 2,
+            'cubeoutput': 1, 'vdw': 0,  'pbelec': 1,
+            'novdwgrid': 1, 'solvenergy': 1, 'dielc': 1, 'dielsol': 80
+        }
+
+    elif execution_type == 'energy':
+        grid_dict = params_grid(grid_type='energy', readgrid= 0,
+                                grid_cen=(47.266, 83.4265, 54.174),
+                                grid_dim=(46, 30, 37),
+                                grid_int=(0.5, 0.5, 0.5))
+        probe_grid_dict = probe_params_grid(probe_id= 0, readgrid= 2, pbfocus= 1, perfill= 0.6,
+                                            grid_int=(1.5, 1.5, 1.5))
+        params_dict = {
+            'title': 'Docking Interaction energy calculation. PB electrostatics',
+            'tipcalc': 3,
+            'calcgrid': 1,
+            'irest': 0,
+            'orest': 0,
+            'coorfmt': 2,
+            'fvdw': 0.8, 'pbelec': 1, 'pbinic': 2, 'wgp': 0, 'ebyatom': 1
+        }
+
+    elif execution_type == 'docking':
+        grid_dict = params_grid(grid_type='docking', readgrid= 0,
+                                grid_cen=(47.266, 83.4265, 54.174),
+                                grid_dim=(46, 30, 37),
+                                grid_int=(0.5, 0.5, 0.5))
+
+        params_dict = {
+            'title': 'Docking Mehler Solmajer dielectric',
+            'tipcalc': 2,
+            'calcgrid': 1,
+            'irest': 0,
+            'orest': 1,
+            'coorfmt': 2,
+            'fvdw': 0.8, 'dields': 2, 'focus': 1, 'cutfoc': 100,
+            'tiprot': 5, 'inifoc': 5, 'ninter': 20,
+            'clhost': 1, 'minout': 50, 'splitpdb': 0
+        }
+
+    elif execution_type == 'docking_rst':
+        params_dict = {
+            'title': 'Docking from restart file',
+            'readgrid': 0,
+            'tipcalc': 2,
+            'calcgrid': 1,
+            'irest': 2,
+            'orest': 1,
+            'coorfmt': 2,
+            'fvdw': 0.8, 'dields': 2, 'focus': 1, 'cutfoc': 100,
+            'tiprot': 5, 'inifoc': 5, 'ninter': 20,
+            'clhost': 1, 'minout': 50, 'splitpdb': 0, 'cutelec': 10.0
+        }
+
+    return {**params_dict, **grid_dict, **probe_grid_dict}
 
 
 def read_params_file(input_params_path: str) -> Dict[str, str]:
