6770a84769c2b967e112816179191ecb1b3a7a4c
gbayarri <genis.bayarri@irbbarcelona.org>
2022-10-20 12:07:40 +0200

    Fixing Read the Docs bug

--- biobb_cmip/docs/source/_static/theme_overrides.js
+++ biobb_cmip/docs/source/_static/theme_overrides.js
@@ -4,5 +4,6 @@ $(document).ready(function() {
 	if($('.icon.icon-home').attr('href') == '../index.html') path = '../';
 	else if($('.icon.icon-home').attr('href') == '../../index.html') path = '../../';
 		
-	$('.icon.icon-home').html('<img src="' + path + '_static/logo.png" class="logo" alt="Logo">');
+	//$('.icon.icon-home').html('<img src="' + path + '_static/logo.png" class="logo" alt="Logo">');
+	$('.icon.icon-home').html('Docs');
 });
