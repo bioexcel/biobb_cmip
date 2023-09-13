100c6c61ae35853e156942c835a148461d5bd744
gbayarri <genis.bayarri@irbbarcelona.org>
2023-09-13 16:56:54 +0200

    2023.3

--- biobb_cmip/__init__.py
+++ biobb_cmip/__init__.py
@@ -1,3 +1,3 @@
 name = "biobb_cmip"
 __all__ = ["cmip"]
-__version__ = "4.0.0"
+__version__ = "4.1.0"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_cmip",
-    version="4.0.0",
+    version="4.1.0",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="biobb_cmip is the Biobb module collection to compute classical molecular interaction potentials.",
@@ -20,11 +20,11 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['docs', 'test']),
     include_package_data=True,
     install_requires=[
-        'biobb_common==4.0.0',
-        'mdanalysis==2.0.0',
-        'biobb_structure_checking==3.13.0'
+        'biobb_common==4.1.0',
+        'mdanalysis>=2.0.0',
+        'biobb_structure_checking==3.13.4'
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     entry_points={
         "console_scripts": [
             "cmip_run = biobb_cmip.cmip.cmip:main",
@@ -36,9 +36,9 @@ setuptools.setup(
     },
     classifiers=(
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
