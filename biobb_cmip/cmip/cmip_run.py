3b8ce4b95ddd87c29be832449f0e06f57e98339e
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-12-22 11:43:11 +0100

    2025.2

--- biobb_cmip/cmip/cmip_run.py
+++ biobb_cmip/cmip/cmip_run.py
@@ -285,9 +285,7 @@ class CmipRun(BiobbObject):
         self.copy_to_host()
 
         # remove temporary folder(s)
-        self.tmp_files.extend([
-            combined_params_dir
-        ])
+        self.tmp_files.append(combined_params_dir)
         self.remove_tmp_files()
 
         self.check_arguments(output_files_created=True, raise_exception=False)
--- biobb_cmip/cmip/cmip_titration.py
+++ biobb_cmip/cmip/cmip_titration.py
@@ -154,9 +154,7 @@ class CmipTitration(BiobbObject):
         self.copy_to_host()
 
         # remove temporary folder(s)
-        self.tmp_files.extend([
-            combined_params_dir
-        ])
+        self.tmp_files.append(combined_params_dir)
         self.remove_tmp_files()
 
         self.check_arguments(output_files_created=True, raise_exception=False)
